111525e4b85c7276d7c79e545a026c7afd4f68b9
Callum Laing <callum.laing91@hotmail.co.uk>
2024-03-25 23:54:42 +0000

    changed skill positioning

--- src/components/Skills.svelte
+++ src/components/Skills.svelte
@@ -17,52 +17,52 @@
 <h1 class="skillsTitle">My Toolbox</h1>
 <p>Below are the skills and tools I use to bring projects to life:</p>
 <div class="skillsContainer">
-    <div class="html skillIcons">
+    <div class="skillIcons">
         <img src={html} alt="html icon">
 		<i>HTML5</i>
 	</div>
-	<div class="css skillIcons">
+	<div class="skillIcons">
         <img src={css} alt="css icon">
 		<i>CSS3</i>
 	</div>
-	<div class="js skillIcons">
+	<div class="skillIcons">
         <img src={javascript} alt="javascript icon">
 		<i>JavaScript</i>
 	</div>
-	<div class="svelte skillIcons">
+	<div class="skillIcons">
         <img src={svelte} alt="svelte icon">
 		<i>Svelte</i>
 	</div>
-	<div class="react skillIcons">
+	<div class="skillIcons">
         <img src={react} alt="react icon">
         <i>React</i>
 	</div>
-    <div class="nextjs skillIcons">
+    <div class="skillIcons">
         <img src={nextjs} alt="nextjs icon">
         <i>Next</i>
 	</div>
-    <div class="vite skillIcons">
+    <div class="skillIcons">
         <img src={vite} alt="vite icon">
         <i>Vite</i>
 	</div>
-    <div class="git skillIcons">
+    <div class="skillIcons">
         <img src={git} alt="git icon">
         <i>Git</i>
 	</div>
-    <div class="github skillIcons">
+    <div class="skillIcons">
         <img src={github} alt="github icon">
         <i>Github</i>
 	</div>
 
-    <div class="cloudflare skillIcons">
+    <div class="skillIcons">
         <img src={cloudflare} alt="cloudflare icon">
         <i>Cloudflare</i>
 	</div>
-    <div class="vscode skillIcons">
+    <div class="skillIcons">
         <img src={vscode} alt="vscode icon">
         <i>VS Code</i>
 	</div>
-    <div class="responsive skillIcons">
+    <div class="skillIcons">
         <img src={responsive} alt="responsive icon">
         <i>Responsive Design</i>
 	</div>
@@ -79,11 +79,10 @@
 
 .skillsContainer {
         display: flex;
+        flex-wrap: wrap;
         justify-content: center;
         align-items: center;
         margin-bottom: 100px;
-        flex-wrap: wrap;
-        gap: 50px;
     }
 
 
@@ -93,17 +92,16 @@
         justify-content: center;
         align-items: center;
         gap: 10px;
+        margin-bottom: 50px;
+        width: calc(100% / 10);
+        margin-left: 50px;
+        margin-right: 50px;
     }
 
 
 img {
     width: 50px;
 }
-
-img:hover {
-    rotate:calc(360);
-}
-
 p {
     font-style: italic;
 }
