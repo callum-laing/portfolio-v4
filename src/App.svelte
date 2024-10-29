455b60f9c9d3f4b07fea5a62d4591a6f807c0c45
Callum Laing <callum.laing91@hotmail.co.uk>
2024-10-29 22:04:45 +0000

    completely re-vamped the hero section of the portfolio

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,15 @@
 <script>
   import "./app.css";
 
-  // import Background from "./components/Background.svelte";
-  import Navbar from "./components/Navbar.svelte";
   import Hero from "./components/Hero.svelte";
   import Projects from "./components/Projects.svelte";
-  import ContactMe from "./components/ContactMe.svelte";
+  import Contact from "./components/Contact.svelte";
 </script>
 
 <main>
-  <!-- <Background /> -->
-  <Navbar />
   <Hero />
   <Projects />
-  <ContactMe />
+  <!-- <Contact /> -->
 </main>
 
 <style>
--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,7 +1,4 @@
 <script>
-  import cal from "../assets/cal.jpg";
-  import callum from "../assets/callum.jpg";
-
   function scrollToElement(event) {
     event.preventDefault();
     const targetId = event.target.getAttribute("href").substring(1);
@@ -15,94 +12,66 @@
   }
 </script>
 
-<!-- <div id="heroWrapper">
-  <div class="intro">
-    <h1>CALLUM LAING</h1>
-    <p class="subtitle">Front-End Developer in England, UK.</p>
-  </div>
-</div> -->
-
-<div class="heroContainer">
-  <div class="aboutHero">
-    <p>
-      I'm a front-end developer based in the UK, passionate about creating
-      dynamic, user-friendly websites. With the same drive and teamwork I bring
-      to American football and rugby, I build sleek, responsive interfaces that
-      perform just as well under pressure. Let's connect and tackle new
-      challenges together!
-    </p>
-
-    <p>
-      It's an exciting journey, and since recently retiring from many years of
-      playing Rugby and American Football, it's been fulfilling redirecting that
-      energy and passion into Web Development.
-    </p>
-    <h1>Callum Laing</h1>
-  </div>
-  <div class="imgHero">
-    <img class="cal" src={cal} alt="" />
-    <img class="callum" src={callum} alt="" />
-  </div>
-</div>
+<section class="hero">
+  <h1>Callum Laing</h1>
+  <nav>
+    <a href="">Projects</a>
+    <a href="">About</a>
+    <a href="">Contact</a>
+  </nav>
+</section>
 
 <style>
-  .heroContainer {
-    display: grid;
-    place-items: center;
-    grid-template-columns: 1fr 1fr;
-    padding: 13em;
-  }
-
-  .imgHero {
-    --box-size: 100px;
-    display: inline-grid;
-    padding-right: calc(var(--box-size) / 2);
-    padding-bottom: calc(var(--box-size) / 2);
-    & > * {
-      grid-area: 1 / 1;
-    }
-  }
-
-  .imgHero > img {
-    width: calc(var(--box-size) * 4);
-    height: calc(var(--box-size) * 4);
-    border: 1px solid black;
-    box-shadow: 2px 2px 2px;
-  }
-
-  .cal {
-    transform: translateX(70%);
-    background-color: rgba(230, 232, 232);
-  }
-  .callum {
-    transform: translateY(50%);
-    background-color: rgba(230, 232, 232);
+  .hero {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    min-height: 100dvh;
+    background: linear-gradient(
+      to bottom,
+      rgb(26, 25, 25) 0%,
+      rgb(26, 25, 25) 15%,
+      rgb(181, 125, 125) 50%,
+      rgb(181, 125, 125) 50%,
+      rgb(26, 25, 25) 85%
+    );
+    padding-bottom: 100px;
   }
 
   h1 {
-    font-size: 4em;
-    margin: 0;
-    font-family: "Herr Von Muellerhoff", cursive;
+    font-size: 8rem;
+    font-family: "Kaushan Script", cursive;
     font-weight: 400;
     font-style: normal;
+    color: black;
+    text-shadow:
+      -1px -1px 0 rgba(255, 255, 255, 0.2),
+      1px -1px 0 rgba(255, 255, 255, 0.2),
+      -1px 1px 0 rgba(255, 255, 255, 0.2),
+      1px 1px 0 rgba(255, 255, 255, 0.2);
   }
 
-  p {
-    font-size: 1.4rem;
-    font-family: "Cormorant Garamond", serif;
-    font-weight: 500;
-    font-style: normal;
+  nav {
+    display: flex;
+    justify-content: center;
+    width: 60%;
   }
 
-  a {
+  nav a {
+    font-size: 2rem;
+    margin: 0 20px;
     text-decoration: none;
     color: black;
-    margin-top: 25px;
+    font-family: "Inknut Antiqua", serif;
+    font-weight: 300;
+    font-style: normal;
+    line-height: 1;
+    transition:
+      font-weight 1s eaase,
+      color 0.3s ease;
   }
-
-  @media (max-width: 768px) {
-  }
-
-  @media (min-width: 768px) and (max-width: 1024px) {
+  nav a:hover {
+    font-weight: bold;
   }
 </style>
