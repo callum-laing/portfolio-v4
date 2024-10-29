13c3db8550430da6cde4f34c5351747ad3085a0a
Callum Laing <callum.laing91@hotmail.co.uk>
2024-10-29 23:24:06 +0000

    added text to about construction

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,16 +1,25 @@
 <section class="about">
-  <h2>About Me</h2>
-  <p>Work in progress...</p>
+  <h2>About</h2>
+  <p>The about section is under construction... please check back later.</p>
 </section>
 
 <style>
   .about {
     min-height: 100dvh;
     background-color: rgb(26, 25, 25);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
   }
 
   h2 {
     text-align: center;
     font-size: 5em;
+  }
+
+  p {
+    font-size: 3rem;
+    text-align: center;
   }
 </style>
